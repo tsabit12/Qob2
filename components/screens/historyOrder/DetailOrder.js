2b3667fcb6804e032f5f68e90467587b8544d13a
tsabit12 <tsabit830@gmail.com>
2020-04-05 00:43:16 +0700

    add animation view detail

--- components/screens/historyOrder/DetailOrder.js
+++ components/screens/historyOrder/DetailOrder.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StatusBar, TouchableOpacity, ScrollView, Alert } from "react-native";
+import { View, Text, StatusBar, TouchableOpacity, ScrollView, Alert, Animated } from "react-native";
 import { connect } from "react-redux";
 import styles from "./styles";
 import { Icon, TopNavigation, TopNavigationAction, ListItem, CheckBox, Button } from '@ui-kitten/components';
@@ -34,56 +34,107 @@ const capitalize = (string) => {
 	}
 }
 
-const PickupView = ({ data, visible, showDetail, checked, onCheckedChange, onPickup, location }) => {
+//loop component
+const DetailComponent = ({ x }) => {
+	const [bounceValue] = React.useState(new Animated.Value(100));
+	var toValue 	= 100;
+	var isHidden 	= true;
+	if (isHidden) {
+		toValue = 0
+	}
+	
+	React.useEffect(() => {
+	    Animated.spring(
+	      bounceValue,
+	      {
+	        toValue: toValue,
+	        velocity: 3,
+	        tension: 2,
+	        friction: 8,
+	      }
+	    ).start();
+	}, []);
+
 	return(
-		<View>
+		<Animated.View style={{ transform: [{translateY: bounceValue}] }}>
+			<View style={{marginLeft: 15, paddingBottom: 5}}>
+				<View>
+	    			<Text style={styles.detailTitle}>Isi Kiriman</Text>
+	    			<Text style={styles.subTitleText}>{capitalize(x.isi_kiriman)}</Text>
+	    		</View>
+	    		<View>
+	    			<Text style={styles.detailTitle}>Nama Pengirim</Text>
+	    			<Text style={styles.subTitleText}>{capitalize(x.nm_pengirim)}</Text>
+	    		</View>
+	    		<View>
+	    			<Text style={styles.detailTitle}>Nama Penerima</Text>
+	    			<Text style={styles.subTitleText}>{capitalize(x.nm_penerima)}</Text>
+	    		</View>
+	    		<View>
+	    			<Text style={styles.detailTitle}>Alamat Pengirim</Text>
+	    			<Text style={styles.subTitleText}>{capitalize(x.alamat_asal)}</Text>
+	    		</View>
+	    		<View>
+	    			<Text style={styles.detailTitle}>Alamat Penerima</Text>
+	    			<Text style={styles.subTitleText}>{capitalize(x.alamat_tujuan)}</Text>
+	    		</View>
+	    		<View>
+	    			<Text style={styles.detailTitle}>Berat</Text>
+	    			<Text style={styles.subTitleText}>{x.berat} gram</Text>
+	    		</View>
+	    		<View>
+	    			<Text style={styles.detailTitle}>Diametrik</Text>
+	    			<Text style={styles.subTitleText}>P = {x.dimensi_p}, L = {x.dimensi_l}, T = {x.dimensi_t} </Text>
+	    		</View>
+			</View>
+		</Animated.View>
+	);
+}
+
+const PickupView = ({ data, visible, showDetail, checked, onCheckedChange, onPickup, location }) => {
+	const scrollRef = React.useRef();
+	const [loc, setLoc] = React.useState({});
+
+	const onPressItem = (x) => {
+		showDetail(x);
+		setTimeout(() => {
+			scrollRef.current.scrollTo({
+		        x: 0,
+		        y: loc[x] - 300
+		    });
+		}, 100)
+	} 
+
+	// console.log(loc);
+
+
+	const addLoc = (layout, id_order) => setLoc({...loc, [id_order]: layout })
+	return(
+		<ScrollView ref={scrollRef}>
 			{ data.map((x, i) => 
 				<React.Fragment key={i}>
 					<ListItem 
 						title={x.id_order} 
 						// description={x.orderDate.substring(0, 10)}
 						titleStyle={{color: '#3366ff', fontFamily: 'open-sans-reg'}}
-						onPress={() => showDetail(x.id_order)}
+						onPress={() => onPressItem(x.id_order)}
 						description={x.insert_date}
 						accessory={(e) => renderItemAccessory(e, x.id_order, checked, onCheckedChange )}
 					/> 
-					{ visible[x.id_order] && <View style={{marginLeft: 15, paddingBottom: 5}}>
-						<View>
-			    			<Text style={styles.detailTitle}>Isi Kiriman</Text>
-			    			<Text style={styles.subTitleText}>{capitalize(x.isi_kiriman)}</Text>
-			    		</View>
-			    		<View>
-			    			<Text style={styles.detailTitle}>Nama Pengirim</Text>
-			    			<Text style={styles.subTitleText}>{capitalize(x.nm_pengirim)}</Text>
-			    		</View>
-			    		<View>
-			    			<Text style={styles.detailTitle}>Nama Penerima</Text>
-			    			<Text style={styles.subTitleText}>{capitalize(x.nm_penerima)}</Text>
-			    		</View>
-			    		<View>
-			    			<Text style={styles.detailTitle}>Alamat Pengirim</Text>
-			    			<Text style={styles.subTitleText}>{capitalize(x.alamat_asal)}</Text>
-			    		</View>
-			    		<View>
-			    			<Text style={styles.detailTitle}>Alamat Penerima</Text>
-			    			<Text style={styles.subTitleText}>{capitalize(x.alamat_tujuan)}</Text>
-			    		</View>
-			    		<View>
-			    			<Text style={styles.detailTitle}>Berat</Text>
-			    			<Text style={styles.subTitleText}>{x.berat} gram</Text>
-			    		</View>
-			    		<View>
-			    			<Text style={styles.detailTitle}>Diametrik</Text>
-			    			<Text style={styles.subTitleText}>P = {x.dimensi_p}, L = {x.dimensi_l}, T = {x.dimensi_t} </Text>
-			    		</View>
-					</View> }
+					<View
+						onLayout={event => {
+					        const layout = event.nativeEvent.layout;
+					        addLoc(layout.y, x.id_order);
+					}}>
+						{ visible[x.id_order] &&  <DetailComponent x={x} /> }
+					</View>
 					<View style={{borderBottomWidth: 0.5, borderBottomColor: '#cbccc4'}}/>
 				</React.Fragment>
 			)}
 			<Button style={{margin: 10}} onPress={onPickup}>
 				{ location ? 'AKTIFKAN LOKASI' : 'PICKUP' }
 			</Button>
-		</View>
+		</ScrollView>
 	);
 } 
 
@@ -106,7 +157,8 @@ class DetailOrder extends React.Component{
 	onPressTab2 = () => this.setState({
 		tab1: { color: 'black' },
 		tab2: { color: 'blue'},
-		activePage: 2
+		activePage: 2,
+		visible: {}
 	})
 
 	onPressTab1 = () => this.setState({
@@ -247,6 +299,7 @@ class DetailOrder extends React.Component{
 
 	render(){
 		const { tab1, tab2, activePage } = this.state;
+		
 		return(
 			<View style={{flex: 1}}>
 				<MyStatusBar />
@@ -273,18 +326,15 @@ class DetailOrder extends React.Component{
 						</TouchableOpacity>
 					</View>
 				</View>
-				<ScrollView>
-					{ activePage === 1 ? 
-						<PickupView 
-							data={this.props.pickup} 
-							visible={this.state.visible}
-							showDetail={this.onShowDetail}
-							checked={this.state.checked}
-							onCheckedChange={this.onChecked}
-							onPickup={this.onPickup}
-							location={this.state.rejectLocation}
-						/> : <Text>Oke</Text>}
-				</ScrollView>
+				{ activePage === 1 ? <PickupView 
+						data={this.props.pickup} 
+						visible={this.state.visible}
+						showDetail={this.onShowDetail}
+						checked={this.state.checked}
+						onCheckedChange={this.onChecked}
+						onPickup={this.onPickup}
+						location={this.state.rejectLocation}
+					/> : <Text>Oke</Text> }
 			</View>
 		);
 	}
